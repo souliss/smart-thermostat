c8ecee66618a5f2b339094dcea6c4ae7d2dad991
Tonino <fazioa@gmail.com>
2016-08-21 10:50:53 +0200

    topics alignment some partial bugfix to layout2

--- e05_ESP8266_SST_webconf/preferences.h
+++ e05_ESP8266_SST_webconf/preferences.h
@@ -56,7 +56,7 @@
 // Define the network configuration according to your router settingsuration according to your router settings
 // and the other on the wireless oneless one
 //Only for DYNAMIC_CONNECTION = 0
-#define peer_address  0xAB0F
+#define peer_address  0xAB21
 #define myvNet_subnet 0xFF00
 #define wifi_bridge_address    0xAB01 //gateway
 
@@ -66,34 +66,39 @@
 #define ACTIVATETOPICSPAGE 1 
 #define TOPICSPAGESNUMBER 2 // 1 if you have less than 3 topics subscribed, 2 if you have more than 3 topics subscribed
 
-#define C1_COLOR()  ucg.setColor(255, 255, 255);    // Bianco
+//ucg.setColor(255, 255, 255);    // White
+//ucg.setColor(255, 0, 0); // Red
+//ucg.setColor(255, 255, 102);    // Yellow
+//ucg.setColor(0, 255, 255);   //light blue
+
+#define C1_COLOR()  ucg.setColor(255, 0, 0); // Red
 #define C1_AREA_UNITY "W"
 #define C1_AREA_TEXT " Grid"
 #define C1_AREA_TEXT_COLOR_LAYOUT2() ucg.setColor(255, 0, 0); //red
 
-#define C2_COLOR()  ucg.setColor(0, 255, 255);    // Giallo Chiaro
+#define C2_COLOR()  ucg.setColor(255, 255, 102);    // Yellow
 #define C2_AREA_UNITY "W"
 #define C2_AREA_TEXT " Solar"
 #define C2_AREA_TEXT_COLOR_LAYOUT2() ucg.setColor(255, 0, 0); //red
 
-#define C3_COLOR()  ucg.setColor(255, 255, 102);    // Giallo Chiaro
-#define C3_AREA_UNITY "°" 
-#define C3_AREA_TEXT " Boil."
+#define C3_COLOR()  ucg.setColor(0, 255, 255);   //light blue
+#define C3_AREA_UNITY "^C" 
+#define C3_AREA_TEXT " Boil"
 #define C3_AREA_TEXT_COLOR_LAYOUT2() ucg.setColor(255, 0, 0); //red
 
-#define C4_COLOR()  ucg.setColor(13, 105, 226);    // Giallo Chiaro
-#define C4_AREA_UNITY "°"
-#define C4_AREA_TEXT " Esterno"
+#define C4_COLOR()  ucg.setColor(255, 255, 255);    // White
+#define C4_AREA_UNITY "'"
+#define C4_AREA_TEXT " topic"
 #define C4_AREA_TEXT_COLOR_LAYOUT2() ucg.setColor(255, 0, 0); //red
 
-#define C5_COLOR()  ucg.setColor(0, 255, 255);    // Giallo Chiaro
-#define C5_AREA_UNITY "W"
-#define C5_AREA_TEXT " Casa"
+#define C5_COLOR()  ucg.setColor(255, 255, 255);    // White
+#define C5_AREA_UNITY "'"
+#define C5_AREA_TEXT " topic"
 #define C5_AREA_TEXT_COLOR_LAYOUT2() ucg.setColor(255, 0, 0); //red
 
-#define C6_COLOR()  ucg.setColor(255, 255, 102);    // Giallo Chiaro
-#define C6_AREA_UNITY "W"
-#define C6_AREA_TEXT " Desk"
+#define C6_COLOR()  ucg.setColor(255, 255, 255);    // White
+#define C6_AREA_UNITY "'"
+#define C6_AREA_TEXT " topic"
 #define C6_AREA_TEXT_COLOR_LAYOUT2() ucg.setColor(255, 0, 0); //red
 
 
